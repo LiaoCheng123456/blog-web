1333256e898c0c63fdfbcd31e19631345c13a108
lc <1337547531@qq.com>
2019-12-20 10:17:50 +0800

    配置文件

--- js/Const.js
+++ js/Const.js
@@ -1,2 +1,2 @@
-// var SERVER_URL = "http://106.54.213.181/blog";
-var SERVER_URL = "http://localhost:8888/";
+var SERVER_URL = "http://106.54.213.181/blog";
+// var SERVER_URL = "http://localhost:8888/";
